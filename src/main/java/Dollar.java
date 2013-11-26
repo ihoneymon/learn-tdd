041e50ca70b8f62a20385e37cbb6fb08bb323303
ihoneymon <ihoneymon@gmail.com>
2013-11-27 00:56:42 +0900

    '3장 모두를 위한 평등'

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -11,5 +11,9 @@ public class Dollar {
         return new Dollar(amount * multiplier);
     }
 
-
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
 }
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -18,5 +20,15 @@ public class SampleTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    /**
+     * 동일성Equality 문제해결
+     * Dollar와 Dollar를 직접비교가능해짐
+     */
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }

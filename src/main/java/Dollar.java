3470e88c8eb4e5d44e6b2d34f5ac9687ac20230f
ihoneymon <ihoneymon@gmail.com>
2013-11-27 00:45:18 +0900

    2장 타락한 객체

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,15 +1,15 @@
 
 public class Dollar {
 
-	public int amount;
+    public int amount;
 
-	public Dollar(int amount) {
-		this.amount = amount;
-	}
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
 
-	public void times(int multiplier) {
-		amount *= multiplier;
-	}
-	
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
 
 }
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -3,14 +3,20 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SampleTest {
-	
-	@Test
-	public  void test() {
-		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
-	}
-	
-	
 
+    @Test
+    public void test() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
 }
